65728a14bfc4e8e0919561cc0da0a0e0b2c1db14
Natali Maevskaya <natali.maevskaya@gmail.com>
2019-08-23 12:59:31 +0300

    Второе задание

--- js/script.js
+++ js/script.js
@@ -1,9 +1,22 @@
 
-let money, 
-    income,
-    addExpenses, 
-    eposit, 
-    mission, 
-    period;
-alert("Мой первый урок.");
-console.log("Всё ещё мой первый урок.");
+let money = 700, 
+    income = 'Decorator',
+    addExpenses = 'Payments, Car, Trips', 
+    deposit = false, 
+    mission = 5000, 
+    period = 12;
+    
+    console.log(typeof(money));
+    console.log(typeof(income));
+    console.log(typeof(deposit));
+
+    console.log(income.length);
+    console.log('Период: ' + period + ' месяцев. ' + 'Цель: заработать ' + mission + ' долларов.');
+
+    console.log((addExpenses.toLowerCase()).split(', '));
+
+    let budgetDay = money / 30;
+    console.log(budgetDay);
+    console.log(money % 30);
+
+    
