fcdea15da0321a091cf4a038f9f07ed80a0cf1aa
Natali Maevskaya <natali.maevskaya@gmail.com>
2019-08-27 17:36:35 +0300

    Урок 4 - Основное задание

--- js/script.js
+++ js/script.js
@@ -1,22 +1,26 @@
 
 'use strict';
 let income = 'Декоратор',
-    mission = 200000;
+    mission = 200000,
+    period = 12;
 let money = prompt('Ваш месячный доход?'); // п.1
 
-let addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую'); // п.2
-if (addExpenses !== null) {
-    console.log((addExpenses.split(', '))); // п.2
-}
+// let addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую'); // п.2
+// if (addExpenses !== null) {
+//     console.log((addExpenses.split(', '))); // п.2
+// }
 
 let deposit = confirm('Есть ли у вас депозит в банке? (ОК - да, Отмена - нет)'); //п.3
 
-// п.4
-console.log(typeof(money));
-console.log(typeof(income));
-console.log(typeof(deposit));
+let showTypeOf = function(data) {
+    console.log(data, typeof(data));
+};
 
-// п.5
+showTypeOf(money);
+showTypeOf(income);
+showTypeOf(deposit);
+
+// Обязательные расходы
 let expenseName1, expenseName2,
     howMuchExp1, howMuchExp2;
 
@@ -29,35 +33,58 @@ do {
     howMuchExp2 = prompt('Во сколько это обойдется?');
 } while(isNaN(howMuchExp2));
 
-// п.6
-let budgetMonth = +money - howMuchExp1 - howMuchExp2;
+let getExpensesMonth = function(exp1, exp2) {
+    if (typeof exp1 !== 'number' || typeof exp2 !== 'number') {
+        exp1 = +exp1;
+        exp2 = +exp2;
+    }
+    return exp1 + exp2;
+};
 
-// п.7
-let numMonthMission = Math.ceil(mission / budgetMonth);
+let getAccumulatedMonth = function(allMoney) {
+    return +allMoney - getExpensesMonth(howMuchExp1, howMuchExp2);
+};
+
+// Накопления за месяц
+let accumulatedMonth = getAccumulatedMonth(money);
+
+// Накопления за период
+console.log('Накопления за ' + period + ' месяцев: ' + accumulatedMonth * period);
+
+let getTargetMonth = function(target, budgetMonth) {
+    return Math.ceil(target / budgetMonth);
+};
+
+// Срок достижения цели
+let numMonthMission = getTargetMonth(mission, accumulatedMonth);
 if ( !isFinite(numMonthMission) || numMonthMission < 0 || isNaN(numMonthMission)) { 
     console.log('Накопить не получится!');
 } else {
     console.log('Накопить ' + mission + ' возможно через ' + numMonthMission + ' месяцев.');
 }
 
+// Дневной бюджет
+let budgetDay = accumulatedMonth / 30;
+// if (budgetDay >= 0) {
+//     console.log('Бюджет на день: ', Math.floor(budgetDay));    
+// } else {
+//     console.log('Бюджет на день: Извините, Вы в минусе!');
+// }
 
-// п.8
-let budgetDay = budgetMonth / 30;
-if (budgetDay >= 0) {
-    console.log('Бюджет на день: ', Math.floor(budgetDay));    
-} else {
-    console.log('Бюджет на день: Извините, Вы в минусе!');
-}
+// Уровень дохода
+let getStatusIncome = function() {
+    if (budgetDay >= 800) {
+        console.log('Высокий уровень дохода');
+    } else if (budgetDay >= 300 && budgetDay < 800) {
+        console.log('Средний уровень дохода');
+    } else if ( budgetDay >= 0 && budgetDay < 300) {
+        console.log('Низкий уровень дохода');
+    } else {
+        console.log('Что-то пошло не так');
+    }
+};
 
-// п.9
-if (budgetDay >= 800) {
-    console.log('Высокий уровень дохода');
-} else if (budgetDay >= 300 && budgetDay < 800) {
-    console.log('Средний уровень дохода');
-} else if ( budgetDay >= 0 && budgetDay < 300) {
-    console.log('Низкий уровень дохода');
-} else {
-    console.log('Что-то пошло не так');
-}
+getStatusIncome();
+
 
     
