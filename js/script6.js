8108090d8bdb953f0a3973fef25790a7e7344b34
Natali Maevskaya <natali.maevskaya@gmail.com>
2019-08-29 20:35:20 +0300

    Урок 6 - доп. зд - Исправления в условиях

--- js/script6.js
+++ js/script6.js
@@ -1,19 +1,16 @@
 let array = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
 
-let date = new Date();
+// let date = new Date();
+let date = new Date('2019-08-25');
 let currentDay = date.getDay();
 
-    for (let i = 0; i < array.length; i++) {
-        if (i === 5 || i === 6) {
-            if (currentDay !== 6 || currentDay !== 0) {
-                document.write(array[i].italics() + '<br>');
-            } else {
-                document.write((array[i].italics()).bold() + '<br>');
-            } 
-        }else if ( i !== (currentDay - 1)) {
-            document.write(array[i] + '<br>');
-        } else {
-            document.write(array[i].bold() + '<br>');            
-        }
-    
-    }
+for (let i = 0; i < array.length; i++) {
+    let currentItem = array[i];
+    if (i === 5 || i === 6) {
+        currentItem = currentItem.italics();
+    } 
+    if ((i + 1) === currentDay || (i === 6 && currentDay === 0)) {
+        currentItem = currentItem.bold();    
+    } 
+    document.write(currentItem + '<br>');           
+}
